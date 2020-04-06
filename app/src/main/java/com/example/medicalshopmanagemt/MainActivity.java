d6532578fa5382b50aced48532c2041ba0286be2
AakankshaKamal <aakankshakamal.post@gmail.com>
2020-04-06 17:36:27 +0530

    validation added

--- app/src/main/java/com/example/medicalshopmanagemt/MainActivity.java
+++ app/src/main/java/com/example/medicalshopmanagemt/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.medicalshopmanagemt;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import 	androidx.appcompat.widget.Toolbar;
 
@@ -21,30 +23,68 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
- Button lobnt;
+ Button lobnt,reset,sendbtn;
 private FirebaseAuth auth;
-private EditText email,pass;
+private EditText email,pass,passwrdreset;
 private String emailid,password;
 ProgressBar progressBar;
+TextView alert;
+
 Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         auth = FirebaseAuth.getInstance();
+reset=findViewById(R.id.pswdreset);
+sendbtn=findViewById(R.id.sendbtn);
         FirebaseUser user = auth.getCurrentUser();
         if(user!=null)
-        {
+        {Toast.makeText(MainActivity.this,"Email :"+user.getEmail(),Toast.LENGTH_LONG).show();
             startActivity(new Intent(MainActivity.this,Dashboard.class));
         }
         progressBar=findViewById(R.id.progessbar);
+        alert=findViewById(R.id.texthere);  passwrdreset=findViewById(R.id.emailtogenerate);
         toolbar=findViewById(R.id.tool);
         email =findViewById(R.id.loginemail);
         pass = findViewById(R.id.psdlo);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            toolbar.setTitle("LOGIN");
+            toolbar.setTitle("ENTER THE FOLLOWING :");
             toolbar.setTitleTextColor(getResources().getColor(R.color.Black));
+
         }
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alert.setVisibility(View.VISIBLE);
+                passwrdreset.setVisibility(View.VISIBLE);
+                //sendbtn.setVisibility(View.VISIBLE);
+
+
+                sendbtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (passwrdreset.getText().toString().trim().equals("")) {
+                            passwrdreset.setError("please enter email address");
+                            passwrdreset.requestFocus();
+                        } else {
+                            FirebaseAuth.getInstance().sendPasswordResetEmail(passwrdreset.getText().toString().trim())
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Log.d("Here", "Email sent.");
+                                                alert.setText("RESET LINK SENT TO GIVEN EMAIL");
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
+
+            }});
+
+
         Button reg = findViewById(R.id.btn);
         reg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,17 +97,29 @@ Toolbar toolbar;
         lobnt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                progressBar.setVisibility(View.VISIBLE);
+          //      progressBar.setVisibility(View.VISIBLE);
+
                 try {
                     emailid = email.getText().toString().trim();
                     password = pass.getText().toString().trim();
+                    if (password.equals("")) {
+                        pass.setError("please enter password");
+                        pass.requestFocus();}
+                        if (emailid.equals("")) {
+                            email.setError("please enter email address");
+                            email.requestFocus();}
                 }
                 catch (NullPointerException e)
                 { Toast.makeText(MainActivity.this,"EMAIL empty:",Toast.LENGTH_LONG).show();}
 
                 Log.d("EMAIL :",emailid);
                 Toast.makeText(MainActivity.this,"EMAIL :"+emailid,Toast.LENGTH_LONG).show();
-               fnc(emailid,password);
+                        if (!emailid.equals("") &&
+                                !password.equals("")
+                                )
+                        {  progressBar.setVisibility(View.VISIBLE);
+                            fnc(emailid,password);
+                        }
 
             }
         });
@@ -80,18 +132,21 @@ void fnc(String emailid,String password)
         public void onComplete(@NonNull Task<AuthResult> task) {
             if(task.isSuccessful())
             {progressBar.setVisibility(View.GONE);
-                Toast.makeText(getApplicationContext(),"LOGEDD IN SUCCESSFULLY",Toast.LENGTH_LONG);
+
+                Toast.makeText(getApplicationContext(),"LOGEDD IN SUCCESSFULLY",Toast.LENGTH_LONG).show();
                 Intent intent=new Intent(MainActivity.this,Dashboard.class);
                 startActivity(intent);
                 finish();
             }
             else
             {
-                Toast.makeText(getApplicationContext(),"LOGING failed",Toast.LENGTH_LONG);
+                Toast.makeText(getApplicationContext(),"LOGING failed",Toast.LENGTH_LONG).show();
             }
 
         }
 
 
     });
-}}
+}
+
+}
--- app/src/main/java/com/example/medicalshopmanagemt/Register.java
+++ app/src/main/java/com/example/medicalshopmanagemt/Register.java
@@ -3,11 +3,14 @@ package com.example.medicalshopmanagemt;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,6 +24,7 @@ public class Register extends AppCompatActivity {
 private EditText emailed;
 private EditText passed;
 private Button clbtn;
+ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +32,7 @@ private Button clbtn;
     mauth=FirebaseAuth.getInstance();
     emailed=findViewById(R.id.email);
     passed=findViewById(R.id.epass);
-
+progressBar=findViewById(R.id.progessbar);
 
         clbtn=findViewById(R.id.click);
 clbtn.setOnClickListener(new View.OnClickListener() {
@@ -38,17 +42,46 @@ clbtn.setOnClickListener(new View.OnClickListener() {
         String email = emailed.getText().toString().trim();
         String password = passed.getText().toString().trim();
 
+        //String emailAddress = etSignInEmail.getText().toString().trim();
+        if (password.length() < 6) {
+            passed.setError("password minimum contain 6 character");
+                    passed.requestFocus();
+        }
+        if (passed.getText().toString().equals("")) {
+            passed.setError("please enter password");
+                    passed.requestFocus();
+        }
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailed.setError("please enter valid email address");
+                    emailed.requestFocus();
+        }
+        if (emailed.getText().toString().equals("")) {
+            emailed.setError("please enter email address");
+                    emailed.requestFocus();
+        }
+        if (!email.equals("") &&
+                passed.getText().toString().length() >= 6 &&
+                !passed.getText().toString().trim().equals("")
+                && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            // do  your action
+
+            progressBar.setVisibility(View.VISIBLE);
+
+
 
         mauth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                    Toast.makeText(Register.this,"DONE",Toast.LENGTH_LONG).show();
-                            Log.d("Success", "createUserWithEmail:success");
-                            FirebaseUser user = mauth.getCurrentUser();
-                           // updateUI(user);
 
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(Register.this,"REGISTERED SUCCESSFULLY",Toast.LENGTH_LONG).show();
+                            Log.d("Success", "createUserWithEmail:success");
+                            //FirebaseUser user = mauth.getCurrentUser();
+                           // updateUI(user);
+startActivity(new Intent(Register.this, Dashboard.class));
+finish();
                 }
                 else
                 {
@@ -59,8 +92,9 @@ clbtn.setOnClickListener(new View.OnClickListener() {
         });
 
 
-    }});
 
 
-    }
 }
+    }});
+}}
+
